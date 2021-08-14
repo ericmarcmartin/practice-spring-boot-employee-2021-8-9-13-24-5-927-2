ce91ded0de49786ee4e3517b28310e5c25b6804f
martier <martier@oocl.com>
2021-08-14 22:43:06 +0800

    [SpringBootCompany][Eric] test: add should_return_all_companies_when_call_get_companies_api

---
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -0,0 +1,91 @@
+package com.thoughtworks.springbootemployee.integration;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+import static java.util.Collections.emptyList;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @AfterEach
+    void tearDown() {
+        companyRepository.deleteAll();
+    }
+
+    @Test
+    void should_return_all_companies_when_call_get_companies_api() throws Exception {
+        // given
+        Company company = companiesDataFactory().get(0);
+        companyRepository.save(company);
+
+        // when
+        // then
+        mockMvc.perform(get("/companies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].companyName").value("Alibaba"))
+                .andExpect(jsonPath("$[0].employees").isArray());
+
+    }
+
+    private List<Company> companiesDataFactory() {
+        List<Company> companies = new ArrayList<>();
+        List<Employee> employees1 = new ArrayList<>();
+        employees1.add(employeesDataFactory().get(0));
+        employees1.add(employeesDataFactory().get(1));
+        employees1.add(employeesDataFactory().get(2));
+
+
+        List<Employee> employees2 = new ArrayList<>();
+        employees2.add(employeesDataFactory().get(3));
+        employees2.add(employeesDataFactory().get(4));
+        employees2.add(employeesDataFactory().get(5));
+
+        companies.add(new Company(1, "Alibaba", employees1));
+        companies.add(new Company(2, "Shoppee", employees2));
+
+        return companies;
+    }
+
+    private List<Employee> employeesDataFactory() {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "Francis", 24, "male", 99));
+        employees.add(new Employee(2, "Eric", 22, "male", 99));
+        employees.add(new Employee(3, "Spongebob", 24, "male", 99));
+        employees.add(new Employee(4, "Patrick", 22, "male", 99));
+        employees.add(new Employee(5, "Gary", 24, "male", 99));
+        employees.add(new Employee(6, "Squidward", 22, "male", 99));
+        employees.add(new Employee(6, "Sandy", 22, "female", 99));
+
+        return employees;
+    }
+}
